4fe90832d67f9eb5e049ad16dcdd3fd8e98161a6
antlas <anton_lastochkin@mail.ru>
2025-07-26 15:16:07 +0300

    42

--- Source/LeaveMe/Private/LMADefaultCharacter.cpp
+++ Source/LeaveMe/Private/LMADefaultCharacter.cpp
@@ -10,10 +10,18 @@ ALMADefaultCharacter::ALMADefaultCharacter()
 {
 	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>("SpringArm"); //создаем компопнет через систему UE с автосборкой мусора
 	SpringArmComponent->SetupAttachment(GetRootComponent()); //устанавливаем рута
-	
+	//дублируемв C++ из блюпринта настройки, для будущего управления через C++
+	//Они будут заданы как дефолтные для блюпринта их можно поменять и вернутся к ним потом
+	SpringArmComponent->SetUsingAbsoluteRotation(true);
+	SpringArmComponent->TargetArmLength = ArmLength; //длина штатива
+	SpringArmComponent->SetRelativeRotation(FRotator(YRotation, 0.0f, 0.0f)); //pitch/yaw/roll
+	SpringArmComponent->bDoCollisionTest = false; //чтобы камера не приближалась при ударе об стену
+	SpringArmComponent->bEnableCameraLag = true; //плавное движение камеры за персонажем, без дерганий и эпилепсии
 
 	CameraComponent = CreateDefaultSubobject<UCameraComponent>("CameraComponent"); //создаем компопнет через систему UE с автосборкой мусора
 	CameraComponent->SetupAttachment(SpringArmComponent); //устанавливаем рута
+	CameraComponent->SetFieldOfView(FOV);
+	CameraComponent->bUsePawnControlRotation = false; //запрещаем камере вращаться относительно SpringArmComponent.
 	
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
--- Source/LeaveMe/Public/LMADefaultCharacter.h
+++ Source/LeaveMe/Public/LMADefaultCharacter.h
@@ -20,7 +20,6 @@ public:
 	ALMADefaultCharacter();
 
 protected:
-protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
 	USpringArmComponent* SpringArmComponent; //рычаг камеры
 
@@ -36,5 +35,9 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
+	
+private:
+    float YRotation = -75.0f;
+	float ArmLength = 1400.0f;
+	float FOV = 55.0f;
 };
