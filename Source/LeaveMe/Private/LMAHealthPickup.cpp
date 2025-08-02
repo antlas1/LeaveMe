2b67fcf5a13373576441b07a1681effc81cc196a
antlas <anton_lastochkin@mail.ru>
2025-08-02 21:21:41 +0300

    slide 109 FULL task

---
+++ Source/LeaveMe/Private/LMAHealthPickup.cpp
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LMAHealthPickup.h"
+#include "Components/LMAHealthComponent.h"
+#include "Components/SphereComponent.h"
+#include "LMADefaultCharacter.h"
+
+// Sets default values
+ALMAHealthPickup::ALMAHealthPickup()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SphereComponent = CreateDefaultSubobject<USphereComponent>("SphereComponent");
+	SphereComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	SphereComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+	SetRootComponent(SphereComponent);
+}
+
+// Called when the game starts or when spawned
+void ALMAHealthPickup::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ALMAHealthPickup::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+	const auto Charcter = Cast<ALMADefaultCharacter>(OtherActor);
+	if (GivePickup(Charcter))
+	{
+		PickupWasTaken();
+	}
+}
+
+// Called every frame
+void ALMAHealthPickup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+bool ALMAHealthPickup::GivePickup(ALMADefaultCharacter* Character)
+{
+	const auto HealthComponent = Character->GetHealthComponent();
+	if (!HealthComponent) return false;
+
+	return HealthComponent->AddHealth(HealthFromPickup);
+}
+
+void ALMAHealthPickup::PickupWasTaken()
+{
+	SphereComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	GetRootComponent()->SetVisibility(false,true);
+
+	FTimerHandle RespawnTimerHandle;
+	GetWorldTimerManager().SetTimer(RespawnTimerHandle, this, &ALMAHealthPickup::RespawnPickup, RespawnTime);
+}
+
+void ALMAHealthPickup::RespawnPickup()
+{
+	SphereComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+	GetRootComponent()->SetVisibility(true,true);
+}
---
+++ Source/LeaveMe/Public/LMAHealthPickup.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "LMAHealthPickup.generated.h"
+
+class USphereComponent;
+class ALMADefaultCharacter;
+
+UCLASS()
+class LEAVEME_API ALMAHealthPickup : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ALMAHealthPickup();
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category="Pickup")
+	USphereComponent* SphereComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Pickup")
+	float RespawnTime = 5.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Pickup", meta = (ClampMin="5.0", ClampMax="100.0"))
+	float HealthFromPickup = 100.0f;
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+private:
+	bool GivePickup(ALMADefaultCharacter* Character);
+	void PickupWasTaken();
+	void RespawnPickup();
+};
